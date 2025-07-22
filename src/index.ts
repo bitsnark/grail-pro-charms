11f82f896f1145d8e17cce950f1c50208fe46ab1
Derek Matter <dharkmattr@gmail.com>
2025-07-22 17:49:01 +0300

    works

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { createPeginSpell } from './api/create-pegin-spell';
 import { createUpdateNftSpell } from './api/create-update-nft-spell';
-import { getPreviousGrailState, injectSignaturesIntoSpell, signAsCosigner, transmitSpell } from './api/spell-operations';
+import { getPreviousGrailState, getPreviousTransactions, injectSignaturesIntoSpell, signAsCosigner, transmitSpell } from './api/spell-operations';
 import { Context } from './core/context';
 
 export {
   Context,
   getPreviousGrailState,
-  injectSignaturesIntoSpell,
-  transmitSpell,
+  getPreviousTransactions,
   createUpdateNftSpell,
   createPeginSpell,
-  signAsCosigner
+  signAsCosigner,
+  injectSignaturesIntoSpell,
+  transmitSpell
 };
