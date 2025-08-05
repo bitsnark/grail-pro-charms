3aa6cce08e191c7dcfbc5059dd315cc7458dcea5
elmol <eliasmolini@gmail.com>
2025-08-05 17:46:08 -0300

    BE: charms-sdk test - #3 -   update  troubleshooting

--- tests/update-e2e.test.ts
+++ tests/update-e2e.test.ts
@@ -7,21 +7,25 @@ import { BitcoinClient } from "../src/core/bitcoin";
 jest.setTimeout(1200000);
 
 // Cosigner constants
+//3
 const COSIGNER_0 = {
   publicKey: 'daa3808e8962acad07bedbbcb94ef7d0f7551cc5188e5dd35eae2dd60d0b8c4f',
   privateKey: '8fea1c500f8414dcc513c4931bc1e1684ce2d3bbe29dbd3b65f7d28fa491a6d8'
 };
 
+//1
 const COSIGNER_1 = {
   publicKey: '5e5479eb816efb7c90ac4134bc84d0b9aae8a5bcad9c576ec096a7419772cce7',
   privateKey: 'adebcf9c8b25776294f15a95878850c07c402c9fa4e2bcd30a9fe008211a5bc8'
 };
 
+//0
 const COSIGNER_2 = {
   publicKey: '52e47c861585d68c876771a581cf523a04a6621e06c3e9876c0151237755ec5f',
   privateKey: '9a130b7fea1ce168861aa4e1a0a54a212836434ee7c5b1721156b17e4695f1ee'
 };
 
+//2
 const COSIGNER_3 = {
   publicKey: '9ca7db41e7ee352ea9f4d3021029e7f0e24a525b2d70ee49b23c82f76d7b577b',
   privateKey: '1cd377ff0666e4f16922910dfad570199c257ca72c19418dd47eac3701edf548'
