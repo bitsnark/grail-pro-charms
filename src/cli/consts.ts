01a1a3a28699a49ce1cccf53c006c617213a1e28
Derek Matter <dharkmattr@gmail.com>
2025-08-12 16:41:21 +0300

    fixed build script

--- src/cli/consts.ts
+++ src/cli/consts.ts
@@ -1,4 +1,4 @@
 export const TIMELOCK_BLOCKS = 100; // Number of blocks to wait before a user payment can be returned to the user
-export const ZKAPP_BIN = './zkapp/target/charms-app'; // Path to the compiled zkApp binary
+export const ZKAPP_BIN = './zkapp/bin/charms-app'; // Path to the compiled zkApp binary
 export const DEFAULT_FEERATE = 0.00002; // Default fee rate in BTC per byte
 export const LOCKED_BTC_MIN_AMOUNT = 1001; // Minimum amount of locked BTC in satoshis
